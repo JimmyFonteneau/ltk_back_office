d1fc1c7671fa1310736c6409d9e33381c8ee708c
unknown <damien.fauveau@outlook.com>
2020-03-17 18:09:41 +0100

    Added PWA

--- los_artistos/urls.py
+++ los_artistos/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path(r'artworks/', include('artworks.urls', namespace='artworks')),
     path(r'users/', include('users.urls', namespace='users')),
     path(r'', users.views.myaccount),
+    path('', include('pwa.urls')),
 ]
