e411352cb4f4cc1bdcc31228bb2524b5cc9fe4c6
unknown <damien.fauveau@outlook.com>
2020-04-21 10:48:00 +0200

    Added static concept page

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+release: python manage.py migrate
 web: gunicorn los_artistos.wsgi --log-file -
--- los_artistos/urls.py
+++ los_artistos/urls.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 import artworks, users, homepage, artists, carts
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(r'concept/', TemplateView.as_view(template_name='concept.html'),name='concept'),
     path(r'artworks/', include('artworks.urls', namespace='artworks')),
     path(r'users/', include('users.urls', namespace='users')),
     path(r'artists/', include('artists.urls', namespace='artists')),
