875ab8bbb183e5c28c2f45789abe6bbae01a96d7
Jimmy Fonteneau <jimmyfo1307@yahoo.fr>
2020-06-22 16:22:59 +0200

    Modification du formulaire de contact

--- los_artistos/forms.py
+++ los_artistos/forms.py
@@ -1,7 +1,19 @@
 from django import forms
 
 class ContactForm(forms.Form):
+    firstName = forms.CharField(
+        required=True
+    )
+    lastName = forms.CharField(
+        required=True
+    )
     email = forms.EmailField(
+        required=True
+    )
+    company = forms.CharField(
+        required=True
+    )
+    phone = forms.CharField(
         required=True
     )
     message = forms.CharField(
