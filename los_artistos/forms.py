a5e655c9593f9a441d3c82a7fa7c00cdb1ec7715
Damien <damien.fauveau@outlook.com>
2020-07-10 15:50:57 +0200

    Translations + integration

--- los_artistos/forms.py
+++ los_artistos/forms.py
@@ -2,22 +2,33 @@ from django import forms
 
 class ContactForm(forms.Form):
     firstName = forms.CharField(
+        label="Prénom",
+        max_length=256,
         required=True
     )
     lastName = forms.CharField(
+        label="Nom",
+        max_length=256,
         required=True
     )
     email = forms.EmailField(
+        label="Email",
+        max_length=256,
         required=True
     )
     company = forms.CharField(
+        label="Entreprise",
+        max_length=256,
         required=True
     )
     phone = forms.CharField(
+        label="Numéro de Télephone",
+        max_length=256,
         required=True
     )
     message = forms.CharField(
         widget=forms.Textarea,
+        max_length=500,
         required=True
     )
     message.widget.attrs.update({ 'class': 'u-full-width height-p' })
