d88513e3cc1b3b5539beded67121a8b25ed4b07b
Jimmy Fonteneau <jimmyfo1307@yahoo.fr>
2020-08-09 17:49:43 +0200

    Administratoin des styles, catégories et des lieux de stockages

--- artworks/urls.py
+++ artworks/urls.py
@@ -12,4 +12,10 @@ urlpatterns = [
     path('new-style/', views.add_style, name="add_style"),
     path('new-categorie/', views.add_category, name="add_category"),
     path('new-storage-place/', views.add_storage_place, name="add_storage_place"),
+    path('all-style/', views.all_style, name="all_style"),
+    path('update-style-<int:style_id>/', views.update_style, name="update_style"),
+    path('all-category/', views.all_category, name="all_category"),
+    path('update-category-<int:category_id>/', views.update_category, name="update_category"),
+    path('all-storage-place/', views.all_storage_place, name="all_storage_place"),
+    path('update-storage-place-<int:storage_place_id>/', views.update_storage_place, name="update_storage_place"),
 ]
